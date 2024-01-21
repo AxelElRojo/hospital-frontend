6f60f7dcaf58f22dacbffcd9f1ca1f8121ce8248
Axel Morales Torres <axelzkai.123@gmail.com>
2024-01-21 12:31:01 -0600

    Added more methods

--- src/Util/Requests.js
+++ src/Util/Requests.js
@@ -14,4 +14,17 @@ requests.get = (endpoint, data, onSuccess, onError) => {
 		data: data
 	}).then(onSuccess).catch(onError);
 }
+requests.delete = (endpoint, id, onSuccess, onError) => {
+	axios({
+		method: 'delete',
+		url: `${process.env.REACT_APP_API_PROTOCOL}://${process.env.REACT_APP_API_ROOT}:${process.env.REACT_APP_API_PORT}/${endpoint}/${id}`,
+	}).then(onSuccess).catch(onError);
+}
+requests.put = (endpoint, data, onSuccess, onError) => {
+	axios({
+		method: 'put',
+		url: `${process.env.REACT_APP_API_PROTOCOL}://${process.env.REACT_APP_API_ROOT}:${process.env.REACT_APP_API_PORT}/${endpoint}`,
+		data: data
+	}).then(onSuccess).catch(onError);
+}
 export default requests;
